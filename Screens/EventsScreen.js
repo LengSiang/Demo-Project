f8b8d53d3dcd489c0693d000258a956e87c50644
Dulan <lva04.20@ichat.sp.edu.sg>
2022-06-09 16:15:14 +0800

    title bar

--- Screens/EventsScreen.js
+++ Screens/EventsScreen.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { FontAwesome } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function EventsScreen() {
+function EventsScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Events!</Text>
         <FontAwesome name="list" size={50} color="red" />
       </View>
     );
+  }
+
+const Stack = createStackNavigator();
+
+export default function EventsStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="EventsHome" component={EventsScreen}/>
+      </Stack.Navigator>
+    )
   }
   
